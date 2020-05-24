b2d95b3cbc576c32152e1ab5979d7ecd3cca20ae
gkedge <gregory.kedge@mantech.com>
2020-05-24 15:57:30 -0400

    Refactored modules for consistency; updated test_all_the_tests_on_cli.py.

--- run_the_project_main.py
+++ run_the_project_main.py
@@ -3,10 +3,10 @@ from module0 import Module0
 print("Execute The Project.")
 
 
-def main():
+def reusable_function():
     the_prog_mod0: Module0 = Module0()
     return f'Exercise {the_prog_mod0.func0()} on class {the_prog_mod0}.'
 
 
 if __name__ == "__main__":
-    print(main())
+    print(reusable_function())
--- test_the_project_main.py
+++ test_the_project_main.py
@@ -1,8 +1,8 @@
-from the_project_main import main
+from run_the_project_main import reusable_function
 
 
 def test_the_project_main():
-    result: str = main()
+    result: str = reusable_function()
     print(result)
     assert result == ('Exercise module0.py:func0 (with help from: utils.py:util0) on class '
                       'module0.py:Module0.')
