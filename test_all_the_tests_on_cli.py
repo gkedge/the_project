d6d9d9b9434ab447b0ef7ce44d29e799ae577524
meeseeks <gregwork@kedges.com>
2020-06-03 22:15:10 -0400

    Started implementation of SrcDirectoryAndSysPath_2 lesson.

--- test_all_the_tests_on_cli.py
+++ test_all_the_tests_on_cli.py
@@ -119,12 +119,10 @@ class RunningTestCase(NamedTuple):
 
 TESTS: List[TestCasePaths] = [
     TestCasePaths.gen_test_case_paths(PROJECT_PATH, Path('test_module0.py')),
-    # TestCasePaths.gen_test_case_paths(PROJECT_PATH, Path('test_utils.py')),
+    TestCasePaths.gen_test_case_paths(PROJECT_PATH, Path('test_utils.py')),
     TestCasePaths.gen_test_case_paths(PROJECT_PATH, Path('test_package.py')),
     TestCasePaths.gen_test_case_paths(PROJECT_PATH, Path('test_the_project_main_reusable_func.py')),
     TestCasePaths.gen_test_case_paths(PROJECT_PATH, Path('src/run_the_project_main.py')),
-    TestCasePaths.gen_test_case_paths(PROJECT_PATH, Path('run_just_because_you_can.py')),
-    # TestCasePaths.gen_test_case_paths(PROJECT_PATH, Path('run_the_project_main_reusable_func.py')),
 ]
 
 ENV: Dict[str, str] = os.environ.copy()
---
+++ test_utils.py
@@ -0,0 +1,5 @@
+from common import utils
+
+
+def test_util0():
+    assert utils.util0() == 'utils.py:util0'
