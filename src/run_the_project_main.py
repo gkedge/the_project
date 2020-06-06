058df3eff069144ddf5376a1562088783605e209
meeseeks <gregwork@kedges.com>
2020-06-06 16:33:53 -0400

    Access common.utils.util0() through common.__init__.py w/ module.

--- src/run_the_project_main.py
+++ src/run_the_project_main.py
@@ -3,13 +3,11 @@ A simple __main__ to access references in the the_package
 """
 from runtime_syspath import print_syspath
 
-# Why is Pycharm/pylint being dumb here? Answered with a Q on __main__ tests below...
 import the_project
 
 print("Execute The Project.")
 
 
-# On your way down, here's a hint: the name of this function...
 def main_reusable_func() -> str:
     """
     A reusable function that could be run from outside this module.
@@ -19,6 +17,6 @@ def main_reusable_func() -> str:
     return f'Exercise {the_prog_mod0.func0()} on class {the_prog_mod0}.'
 
 
-if __name__ == "__main__":  # <-- What does this if-test do?
+if __name__ == "__main__":
     print_syspath(sort=False)
     print(main_reusable_func())
