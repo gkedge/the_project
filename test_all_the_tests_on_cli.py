807cd94933e39d326b3af84cc294faa944cfcc18
meeseeks <gregwork@kedges.com>
2020-07-28 22:00:55 -0400

    Upgrade test_all_the_tests_on_cli.py

--- test_all_the_tests_on_cli.py
+++ test_all_the_tests_on_cli.py
@@ -6,7 +6,7 @@ Run a series of test invocation making sure each test case works with both
 from pathlib import Path, PurePath
 from typing import Tuple
 
-from run_all_the_tests import TestCase, TestCasePath, TestType, run_all_tests
+from run_all_the_tests import TestCase, TestCasePath, run_all_tests
 
 if __name__ == '__main__':
     project_path: PurePath = Path(__file__).absolute().parent
@@ -16,17 +16,11 @@ if __name__ == '__main__':
         return TestCasePath(project_path, PurePath(test_case))
 
 
-    TESTS: List[TestCasePaths] = [
-        TestCasePaths.gen_test_case_paths(PROJECT_PATH, Path('test_module0.py')),
-        TestCasePaths.gen_test_case_paths(PROJECT_PATH, Path('test_package.py')),
-        TestCasePaths.gen_test_case_paths(PROJECT_PATH, Path('run_the_project_main.py')),
-        TestCasePaths.gen_test_case_paths(PROJECT_PATH, Path('test_the_project_main_reusable_func.py')),
-    ]
-
     all_test_cases: Tuple[TestCase, ...] = (
         TestCase.gen_test_case(gen_test_case_path('test_module0.py')),
+        TestCase.gen_test_case(gen_test_case_path('test_package.py')),
         TestCase.gen_test_case(gen_test_case_path('test_the_project_main_reusable_func.py')),
-        TestCase.gen_test_case(gen_test_case_path('run_the_project_main.py'), test_types=(TestType.PYTHON,)),
+        TestCase.gen_test_case(gen_test_case_path('run_the_project_main.py')),
     )
 
     run_all_tests(all_test_cases)
