6081e5e4ad632d0acb34465d8cb3477712ebd4df
meeseeks <gregwork@kedges.com>
2020-08-02 23:31:28 -0400

    Updated run_all_the_tests

--- run_all_the_tests_on_cli.py
+++ run_all_the_tests_on_cli.py
@@ -6,7 +6,7 @@ Run a series of test invocation making sure each test case works with both
 from pathlib import Path, PurePath
 from typing import Tuple
 
-from run_all_the_tests import TestCase, TestCasePath, run_all_tests, Group
+from run_all_the_tests import TestCase, TestCasePath, run_all_tests
 
 if __name__ == '__main__':
     project_path: PurePath = Path(__file__).absolute().parent
@@ -18,8 +18,8 @@ if __name__ == '__main__':
 
     all_test_cases: Tuple[TestCase, ...] = (
         TestCase.gen_test_case(gen_test_case_path('test_module0.py')),
-        TestCase.gen_test_case(gen_test_case_path('test_utils.py'), Group.TWO),
-        TestCase.gen_test_case(gen_test_case_path('test_file_utils.py'), Group.THREE),
+        TestCase.gen_test_case(gen_test_case_path('test_utils.py'), wait_between_test_types=True),
+        TestCase.gen_test_case(gen_test_case_path('test_file_utils.py'), wait_between_test_types=True),
         TestCase.gen_test_case(gen_test_case_path('test_package.py')),
         TestCase.gen_test_case(gen_test_case_path('test_the_project_main_reusable_func.py')),
         TestCase.gen_test_case(gen_test_case_path('src/run_the_project_main.py')),
