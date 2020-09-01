719618160ecb03b7a41a7870ac36f699a5501b86
meeseeks <gregwork@kedges.com>
2020-08-31 22:03:08 -0400

    Black, isort, pylint & mypy w/ dependency TypeHints
    Type Hints: run-all-the-tests and runtime-syspath

--- run_all_the_tests_on_cli.py
+++ run_all_the_tests_on_cli.py
@@ -11,16 +11,16 @@ from run_all_the_tests import TestCase, TestCasePath, run_all_tests
 if __name__ == "__main__":
     project_path: PurePath = Path(__file__).absolute().parent
 
-
     def gen_test_case_path(test_case: str) -> TestCasePath:
         return TestCasePath(project_path, PurePath(test_case))
 
-
     all_test_cases: Tuple[TestCase, ...] = (
-        TestCase.gen_test_case(gen_test_case_path('test_module0.py')),
-        TestCase.gen_test_case(gen_test_case_path('test_package.py')),
-        TestCase.gen_test_case(gen_test_case_path('test_the_project_main_reusable_func.py')),
-        TestCase.gen_test_case(gen_test_case_path('run_the_project_main.py')),
+        TestCase.gen_test_case(gen_test_case_path("test_module0.py")),
+        TestCase.gen_test_case(gen_test_case_path("test_package.py")),
+        TestCase.gen_test_case(
+            gen_test_case_path("test_the_project_main_reusable_func.py")
+        ),
+        TestCase.gen_test_case(gen_test_case_path("run_the_project_main.py")),
     )
 
     run_all_tests(all_test_cases)
--- test_package.py
+++ test_package.py
@@ -2,4 +2,4 @@ from the_project import Module0
 
 
 def test_func0(fixture0: Module0):
-    assert fixture0.func0() == 'module0.py:func0 (with help from: utils.py:util0)'
+    assert fixture0.func0() == "module0.py:func0 (with help from: utils.py:util0)"
